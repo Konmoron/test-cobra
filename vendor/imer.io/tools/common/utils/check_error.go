9f06750dc39029ef0b2583a6e6dac52692e8862b
Konmoron <cqlq2012@gmail.com>
2019-11-07 12:45:06 +0800

    完善vendor

--- vendor/imer.io/tools/common/utils/check_error.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "fmt"
-
-func CheckErr(errMess error) {
-	if errMess != nil {
-		fmt.Println(errMess)
-	}
-}
--- vendor/imer.io/tools/common/utils/recover_name.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "fmt"
-
-func RecoverName() {
-	if r := recover(); r != nil {
-		fmt.Println("recovered from", r)
-	}
-}
